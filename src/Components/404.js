895d3754b04be9305f9623212f9f71cc606c719a
J_C___ <jcsumlin@pm.me>
2020-08-11 19:50:40 -0400

    Added alt to image

--- src/Components/404.js
+++ src/Components/404.js
@@ -7,7 +7,7 @@ class PageNotFoundError extends Component
     {
         return (
             <div>
-                <img src={PageNotFound}/>
+                <img alt="404 tunic" src={PageNotFound}/>
                 <h1>404 Page Not Found!</h1>
                 <h2>Looks like you went adventuring a little too far.</h2>
                 <NavLink to="/">
