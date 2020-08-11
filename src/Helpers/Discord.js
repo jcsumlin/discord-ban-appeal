512c77c5b3abfc8e8af73b07e49146f57f64d297
J_C___ <jcsumlin@pm.me>
2020-08-11 17:19:25 -0400

    redid some of the ban checks

--- src/Helpers/Discord.js
+++ src/Helpers/Discord.js
@@ -1,14 +1,25 @@
-const API_ENDPOINT = "https://discord.com/api/v6";
 const axios = require("axios")
 
 function userIsBanned(userId, guildId, botToken) {
-    const result = axios.get(`${API_ENDPOINT}/guilds/${encodeURIComponent(guildId)}/bans/${encodeURIComponent(userId)}`, {
-        headers: {
-            "Authorization": `Bot ${botToken}`
-        }
-    });
 
-    return result.ok;
+    var config = {
+        method: 'get',
+        url: `https://discord.com/api/v6/guilds/${encodeURIComponent(guildId)}/bans/${encodeURIComponent(userId)}`,
+        headers: {
+            'Authorization': `Bot ${botToken}`,
+            'Cookie': '__cfduid=d593767d468b7548ad579dc31781b3a101597176898; __cfruid=c124033110a13645fb4a09a2d239403b5bcd1564-1597176898'
+        }
+    };
+    var result = axios(config)
+        .then(function (response) {
+            if (response.status === 200) {
+                return true
+            } else return false;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    return result
 }
 
 module.exports = { userIsBanned };
