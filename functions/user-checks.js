2b728752dab7dd40cea1317e27d3fa9927e3f366
J_C___ <jcsumlin@pm.me>
2020-08-11 21:21:40 -0400

    Updated function

--- functions/user-checks.js
+++ functions/user-checks.js
@@ -6,7 +6,22 @@ exports.handler = async function (event, context) {
             statusCode: 405
         };
     }
-    return {
-        body: event
+    var user_id = event.queryStringParameters.user_id
+    if (user_id !== undefined) {
+        if (process.env.GUILD_ID && !process.env.SKIP_BAN_CHECK) {
+            if (!await userIsBanned(user_id, process.env.REACT_APP_GUILD_ID, process.env.REACT_APP_DISCORD_BOT_TOKEN)) {
+                return {
+                    statusCode: 200,
+                    is_banned: false
+                };
+            }
+            return {
+                statusCode: 200,
+                is_banned: true,
+            };
+        }
     }
+    return {
+        statusCode: 400
+    };
 }
