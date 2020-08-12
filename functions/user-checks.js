4a36d5bcb4ecdb18d492e20f392cd9d0936f89c9
J_C___ <jcsumlin@gmail.com>
2020-08-11 22:00:33 -0400

    Update user-checks.js

--- functions/user-checks.js
+++ functions/user-checks.js
@@ -7,12 +7,13 @@ exports.handler = async function (event, context) {
         };
     }
     var user_id = event.queryStringParameters.user_id
-    return {
-        body: `${user_id} ${process.env.REACT_APP_GUILD_ID}  ${process.env.REACT_APP_DISCORD_BOT_TOKEN}`,
-        statusCode:200
-    }
     if (user_id !== undefined) {
-        if (process.env.GUILD_ID && !process.env.SKIP_BAN_CHECK) {
+        if (process.env.REACT_APP_GUILD_ID && !process.env.REACT_APP_SKIP_BAN_CHECK) {
+            result = await userIsBanned(user_id, process.env.REACT_APP_GUILD_ID, process.env.REACT_APP_DISCORD_BOT_TOKEN)
+            return {
+                body: `${user_id} ${process.env.REACT_APP_GUILD_ID}  ${process.env.REACT_APP_DISCORD_BOT_TOKEN} ${result}`,
+                statusCode:200
+            }
             if (!await userIsBanned(user_id, process.env.REACT_APP_GUILD_ID, process.env.REACT_APP_DISCORD_BOT_TOKEN)) {
                 return {
                     statusCode: 200,
