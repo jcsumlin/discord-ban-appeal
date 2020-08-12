d6c8656fa0cea4f544b5f86f526583569f9352af
J_C___ <jcsumlin@pm.me>
2020-08-11 22:03:16 -0400

    Edited user-checks.js

--- functions/user-checks.js
+++ functions/user-checks.js
@@ -9,11 +9,6 @@ exports.handler = async function (event, context) {
     var user_id = event.queryStringParameters.user_id
     if (user_id !== undefined) {
         if (process.env.REACT_APP_GUILD_ID && !process.env.REACT_APP_SKIP_BAN_CHECK) {
-            result = await userIsBanned(user_id, process.env.REACT_APP_GUILD_ID, process.env.REACT_APP_DISCORD_BOT_TOKEN)
-            return {
-                body: `${user_id} ${process.env.REACT_APP_GUILD_ID}  ${process.env.REACT_APP_DISCORD_BOT_TOKEN} ${result}`,
-                statusCode:200
-            }
             if (!await userIsBanned(user_id, process.env.REACT_APP_GUILD_ID, process.env.REACT_APP_DISCORD_BOT_TOKEN)) {
                 return {
                     statusCode: 200,
