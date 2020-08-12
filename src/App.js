82297a8b562d4348be5f11b37427c4d7c71ab154
J_C___ <jcsumlin@pm.me>
2020-08-12 10:31:14 -0400

    added new lambda function

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import Home from "./Components/Home";
 import Callback from "./Components/Callback";
 import Form from "./Components/Form";
 import {Redirect} from "react-router-dom";
-import logo from "./Images/header.jpg";
 import Grid from "@material-ui/core/Grid";
 import Success from "./Components/Success";
 import Error from "./Components/Error";
