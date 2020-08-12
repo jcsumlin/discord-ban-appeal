ee9eb027d729ec4c0d2bed6a9c37eda3ec5b561e
J_C___ <jcsumlin@gmail.com>
2020-08-11 21:45:45 -0400

    Update user-checks.js

--- functions/user-checks.js
+++ functions/user-checks.js
@@ -7,6 +7,10 @@ exports.handler = async function (event, context) {
         };
     }
     var user_id = event.queryStringParameters.user_id
+    return {
+        body: user_id,
+        statusCode:200
+    }
     if (user_id !== undefined) {
         if (process.env.GUILD_ID && !process.env.SKIP_BAN_CHECK) {
             if (!await userIsBanned(user_id, process.env.REACT_APP_GUILD_ID, process.env.REACT_APP_DISCORD_BOT_TOKEN)) {
