f88fdb942ea98ad8021ef3eaed98bafd3a0adbb0
OviiiOne <oviedo.97@hotmail.com>
2022-01-12 20:29:54 +0100

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -18,7 +18,7 @@ class Home extends Component {
         return (
             <Grid container alignItems={"center"} justify="center" direction="column">
                 <Grid item xs={12}>
-                    <Button startIcon={<Icon size={1} path={mdiDiscord}/>} href={url} size={"large"} className={"button"}>Login with Discord</Button>
+                    <Button startIcon={<Icon size={1} path={mdiDiscord}/>} href={url} size={"large"} className={"button"}>Iniciar sesión con Discord</Button>
                 </Grid>
             </Grid>
         );
