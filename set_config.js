b180b1506a3d0344a815f3147c66924bdd3da96e
Chat Sumlin <chat.sumlin@alteryx.com>
2022-07-05 10:39:37 -0400

    added more logging to build script

--- set_config.js
+++ set_config.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
 const CONFIG_FILE = './src/config.json'
+console.log("reading config file")
 let rawdata = fs.readFileSync(CONFIG_FILE);
 let config_data = JSON.parse(rawdata);
 config_data["repository_url"] = process.env.REPOSITORY_URL
+console.log("repository_url set to:", process.env.REPOSITORY_URL)
 console.log(config_data)
 let string_data = JSON.stringify(config_data);
 fs.writeFileSync(CONFIG_FILE, string_data);
